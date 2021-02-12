2d04ffdd5e402d364124601a7ea82e08faa904b1
Lucas Fellipe Mondini Pereira <lucasfepereira1@gmail.com>
2021-02-12 19:26:19 -0300

    includes for the engine

---
+++ SMENG/SMENG/src/SMENG.h
@@ -0,0 +1,11 @@
+#pragma once
+
+//For use by applications
+
+#include <stdio.h>
+#include <SMENG/Application.h>
+
+
+// ------- Entry Point ----------------
+#include <SMENG/EntryPoint.h>
+// ------- Entry Point ----------------
