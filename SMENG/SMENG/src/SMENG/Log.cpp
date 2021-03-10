903ad04d8bd75fa6013c6305f4ed4bdd6e41cb29
Lucas Fellipe Mondini Pereira <lucasfepereira1@gmail.com>
2021-03-10 19:00:01 -0300

    just removed a space

--- SMENG/SMENG/src/SMENG/Log.cpp
+++ SMENG/SMENG/src/SMENG/Log.cpp
@@ -2,11 +2,13 @@
 #include "spdlog/sinks/stdout_color_sinks.h"
 
 namespace SMENG {
+
 	std::shared_ptr<spdlog::logger> Log::s_CoreLogger;
 	std::shared_ptr<spdlog::logger> Log::s_ClientLogger;
 
 	void Log::Init() {
-		spdlog::set_pattern("%^ [%T] %n: %v%$");
+
+		spdlog::set_pattern("%^[%T] %n: %v%$");
 
 		s_CoreLogger = spdlog::stdout_color_mt("SMENG");
 		s_CoreLogger->set_level(spdlog::level::trace);
