7ceffc804d0ffc30a3920e013e749e21bd3c1a05
Lucas Fellipe Mondini Pereira <lucasfepereira1@gmail.com>
2021-03-10 18:51:25 -0300

    generates a window event for test

--- SMENG/SMENG/src/SMENG/Application.cpp
+++ SMENG/SMENG/src/SMENG/Application.cpp
@@ -1,5 +1,9 @@
 #include "Application.h"
 
+#include "Events/Event.h"
+#include "Events/ApplicationEvent.h"
+#include "SMENG/Log.h"
+
 
 namespace SMENG{
 	Application::Application()
@@ -12,6 +16,9 @@ namespace SMENG{
 
 	void Application::Run()
 	{
+		WindowResizeEvent e(WINDOW_W, WINDOW_H);
+		SMENG_CLIENT_TRACE(e);
+
 		while (true);
 	}
 }
