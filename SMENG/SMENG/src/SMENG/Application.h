3f60498791a931f02f7f3ad48ffc2926b4606792
Lucas Fellipe Mondini Pereira <lucasfepereira1@gmail.com>
2021-03-10 18:52:20 -0300

    added variables to window size

--- SMENG/SMENG/src/SMENG/Application.h
+++ SMENG/SMENG/src/SMENG/Application.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Core.h"
 
+
 namespace SMENG {
 
 	class SMENG_API Application
@@ -10,6 +11,9 @@ namespace SMENG {
 		virtual ~Application();
 
 		void Run();
+	private:
+		int WINDOW_W = 1280;
+		int WINDOW_H = 720;
 	};
 
 	//To be defined in the client
